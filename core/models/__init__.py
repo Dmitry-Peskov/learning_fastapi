ce9f995177f401d843f050650077efec10c8d6dc
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-03-11 23:13:41 +0300

    Поправил импорты. Реализовал функцию, которая при запуске приложения проверяет наличие фалйа БД. Еслие его нет, создаёт её со всеми необходимыми таблицами и связями

---
+++ core/models/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["BaseModel"]
+
+
+from .base_model import BaseModel
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
+from core.database import database
+from core.models import BaseModel
 
 
-app = FastAPI()
+@asynccontextmanager
+async def launch_app(app: FastAPI):
+    async with database.engine.begin() as connection:
+        await connection.run_sync(BaseModel.metadata.create_all)
+    yield
 
+
+app = FastAPI(lifespan=launch_app)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
